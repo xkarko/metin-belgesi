c373474a4c05db94e2ad3d7184ae71053e047865
User <User@LAB2-11>
2024-02-21 09:32:30 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuDosya = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuYeni = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuAc = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKaydet = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuYazdir = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuDuzen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKes = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKopyala = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuYapistir = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCikiss = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCikis = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtEditor = new System.Windows.Forms.RichTextBox();
+            this.belge = new System.Drawing.Printing.PrintDocument();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuDosya,
+            this.menuDuzen,
+            this.menuCikiss});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuDosya
+            // 
+            this.menuDosya.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuYeni,
+            this.menuAc,
+            this.menuKaydet,
+            this.menuYazdir});
+            this.menuDosya.Name = "menuDosya";
+            this.menuDosya.Size = new System.Drawing.Size(51, 20);
+            this.menuDosya.Text = "Dosya";
+            // 
+            // menuYeni
+            // 
+            this.menuYeni.Name = "menuYeni";
+            this.menuYeni.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuYeni.Size = new System.Drawing.Size(180, 22);
+            this.menuYeni.Text = "Yeni";
+            this.menuYeni.Click += new System.EventHandler(this.yeniToolStripMenuItem_Click);
+            // 
+            // menuAc
+            // 
+            this.menuAc.Name = "menuAc";
+            this.menuAc.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.menuAc.Size = new System.Drawing.Size(180, 22);
+            this.menuAc.Text = "Aç";
+            this.menuAc.Click += new System.EventHandler(this.açToolStripMenuItem_Click);
+            // 
+            // menuKaydet
+            // 
+            this.menuKaydet.Name = "menuKaydet";
+            this.menuKaydet.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.menuKaydet.Size = new System.Drawing.Size(180, 22);
+            this.menuKaydet.Text = "Kaydet";
+            this.menuKaydet.Click += new System.EventHandler(this.menuKaydet_Click);
+            // 
+            // menuYazdir
+            // 
+            this.menuYazdir.Name = "menuYazdir";
+            this.menuYazdir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.menuYazdir.Size = new System.Drawing.Size(180, 22);
+            this.menuYazdir.Text = "Yazdır";
+            this.menuYazdir.Click += new System.EventHandler(this.yazdırToolStripMenuItem_Click);
+            // 
+            // menuDuzen
+            // 
+            this.menuDuzen.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuKes,
+            this.menuKopyala,
+            this.menuYapistir});
+            this.menuDuzen.Name = "menuDuzen";
+            this.menuDuzen.Size = new System.Drawing.Size(52, 20);
+            this.menuDuzen.Text = "Düzen";
+            // 
+            // menuKes
+            // 
+            this.menuKes.Name = "menuKes";
+            this.menuKes.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.menuKes.Size = new System.Drawing.Size(158, 22);
+            this.menuKes.Text = "Kes";
+            this.menuKes.Click += new System.EventHandler(this.menuKes_Click);
+            // 
+            // menuKopyala
+            // 
+            this.menuKopyala.Name = "menuKopyala";
+            this.menuKopyala.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.menuKopyala.Size = new System.Drawing.Size(158, 22);
+            this.menuKopyala.Text = "Kopyala";
+            this.menuKopyala.Click += new System.EventHandler(this.menuKopyala_Click);
+            // 
+            // menuYapistir
+            // 
+            this.menuYapistir.Name = "menuYapistir";
+            this.menuYapistir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.menuYapistir.Size = new System.Drawing.Size(158, 22);
+            this.menuYapistir.Text = "Yapıştır";
+            this.menuYapistir.Click += new System.EventHandler(this.menuYapistir_Click);
+            // 
+            // menuCikiss
+            // 
+            this.menuCikiss.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCikis});
+            this.menuCikiss.Name = "menuCikiss";
+            this.menuCikiss.Size = new System.Drawing.Size(44, 20);
+            this.menuCikiss.Text = "Çıkış";
+            this.menuCikiss.Click += new System.EventHandler(this.menuCikiss_Click);
+            // 
+            // menuCikis
+            // 
+            this.menuCikis.Name = "menuCikis";
+            this.menuCikis.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.menuCikis.Size = new System.Drawing.Size(180, 22);
+            this.menuCikis.Text = "Çıkış";
+            this.menuCikis.Click += new System.EventHandler(this.menuCikis_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontToolStripMenuItem,
+            this.colorToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.ContextMenuStrip = this.contextMenuStrip1;
+            this.txtEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtEditor.Location = new System.Drawing.Point(0, 24);
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(800, 426);
+            this.txtEditor.TabIndex = 2;
+            this.txtEditor.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuDosya;
+        private System.Windows.Forms.ToolStripMenuItem menuDuzen;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuYeni;
+        private System.Windows.Forms.ToolStripMenuItem menuAc;
+        private System.Windows.Forms.ToolStripMenuItem menuKaydet;
+        private System.Windows.Forms.ToolStripMenuItem menuYazdir;
+        private System.Windows.Forms.ToolStripMenuItem menuKes;
+        private System.Windows.Forms.ToolStripMenuItem menuKopyala;
+        private System.Windows.Forms.ToolStripMenuItem menuYapistir;
+        private System.Windows.Forms.ToolStripMenuItem menuCikiss;
+        private System.Windows.Forms.ToolStripMenuItem menuCikis;
+        private System.Windows.Forms.RichTextBox txtEditor;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Drawing.Printing.PrintDocument belge;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void yeniToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.Show();
+        }
+
+        private void açToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Dosyaları | *.txt";
+            DialogResult cevap = ofd.ShowDialog();
+            if (cevap == DialogResult.OK)
+            {
+                txtEditor.LoadFile(ofd.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void yazdırToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintDialog pd = new PrintDialog();
+            DialogResult cevap = pd.ShowDialog();
+            if (cevap == DialogResult.OK)
+            {
+                belge.Print();
+            }
+        }
+
+        private void menuKes_Click(object sender, EventArgs e)
+        {
+            txtEditor.Cut();
+        }
+
+        private void menuKopyala_Click(object sender, EventArgs e)
+        {
+            txtEditor.Copy();
+        }
+
+        private void menuYapistir_Click(object sender, EventArgs e)
+        {
+            txtEditor.Paste();
+        }
+        private void belge_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(txtEditor.Text, txtEditor.Font,
+            Brushes.Black, new Point(100, 100));
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                txtEditor.Font = fd.Font;
+            }
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                txtEditor.SelectionColor = cd.Color;
+            }
+        }
+
+        private void menuKaydet_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Dosyası | *.txt | Tüm Dosyalar | *.*";
+            DialogResult cevap = sfd.ShowDialog();
+            if (cevap == DialogResult.OK)
+            {
+                txtEditor.SaveFile(sfd.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void menuCikis_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void menuCikiss_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
